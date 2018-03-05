6441f63eaa37f374d21a0badd0018dbbce1a6342
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2018-03-05 13:53:31 +0100

    finish ui for editAll and readAll

--- studio/main_dev.js
+++ studio/main_dev.js
@@ -9,6 +9,18 @@ Studio.initializeListeners.push(async () => {
   }
 
   Studio.authentication.useEditorComponents.push((user) => <div>
-        Authorization
+    <h2>Authorization</h2>
+    <div>
+      <div className='form-group'>
+        <label>Allow read all entities</label>
+        <input type='checkbox' checked={user.readAllPermissions === true}
+          onChange={(v) => Studio.updateEntity({...user, readAllPermissions: v.target.checked})} />
+      </div>
+      <div className='form-group'>
+        <label>Allow edit all entities</label>
+        <input type='checkbox' checked={user.editAllPermissions === true}
+          onChange={(v) => Studio.updateEntity({...user, editAllPermissions: v.target.checked})} />
+      </div>
+    </div>
   </div>)
 })
