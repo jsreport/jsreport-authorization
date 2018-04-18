b42807850db139e40aedc76047d734c776744702
BJR Matos <bjrmatos@gmail.com>
2018-04-18 12:30:00 -0500

    remove boilerplate options schema since core now adds default one when extension don’t specify one

--- index.js
+++ index.js
@@ -3,16 +3,6 @@ var config = require('./jsreport.config.js')
 
 module.exports = function (options) {
   config.options = options
-
-  config.optionsSchema = {
-    extensions: {
-      authorization: {
-        type: 'object',
-        properties: {}
-      }
-    }
-  }
-
   config.main = main
   config.directory = __dirname
   return config
