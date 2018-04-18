8dae3ae3f4aff62ade5a59f8b3f27c33594d5c24
BJR Matos <bjrmatos@gmail.com>
2018-04-17 19:03:50 -0500

    add options schema and use hide values in sandbox

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var config = require('./jsreport.config.js')
 
 module.exports = function (options) {
   config.options = options
+
+  config.optionsSchema = {
+    extensions: {
+      authorization: {
+        type: 'object',
+        properties: {}
+      }
+    }
+  }
+
   config.main = main
   config.directory = __dirname
   return config
