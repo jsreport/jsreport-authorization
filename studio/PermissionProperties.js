9d2325f3560ae3c00e33b4999c44cc5cbc309c16
BJR Matos <bjrmatos@gmail.com>
2018-12-18 19:35:11 -0500

    use Studio EntityRefSelect control for user selection

--- studio/PermissionProperties.js
+++ studio/PermissionProperties.js
@@ -1,56 +1,46 @@
 import React, { Component } from 'react'
 import Studio from 'jsreport-studio'
 
-const MultiSelect = Studio.MultiSelect
+const EntityRefSelect = Studio.EntityRefSelect
+
+const selectValues = (selected) => {
+  return selected.map((e) => e._id)
+}
 
 export default class PermissionProperties extends Component {
-  selectUsers (entities) {
-    return Object.keys(entities).filter((k) => entities[k].__entitySet === 'users' && !entities[k].__isNew).map((k) => entities[k])
-  }
-
   render () {
-    const { entity, entities, onChange } = this.props
-    const users = this.selectUsers(entities)
+    const { entity, onChange } = this.props
 
     if (entity.__entitySet === 'users') {
       return <div />
-    }
-
-    const selectValues = (selectData) => {
-      const { value: selectedValue, options } = selectData
-      let res = []
-
-      for (var i = 0; i < options.length; i++) {
-        const optionIsSelected = selectedValue.indexOf(options[i].value) !== -1
-
-        if (optionIsSelected) {
-          res.push(options[i].value)
-        }
-      }
-
-      return res
     }
 
     return (
       <div className='properties-section'>
         <div className='form-group'>
           <label>read permissions</label>
-          <MultiSelect
-            title='Use the checkboxes to select/deselect multiple options'
-            size={7}
-            value={entity.readPermissions || []}
-            onChange={(selectData) => onChange({ _id: entity._id, readPermissions: selectValues(selectData) })}
-            options={users.map(u => ({ key: u._id, name: u.username, value: u._id }))}
+          <EntityRefSelect
+            headingLabel='Select user (read permissions)'
+            filter={(references) => {
+              const users = references.users.filter((e) => !e.__isNew)
+              return { users: users }
+            }}
+            value={entity.readPermissions ? entity.readPermissions.map((_id) => Studio.getEntityById(_id, false).shortid) : []}
+            onChange={(selected) => onChange({ _id: entity._id, readPermissions: selectValues(selected) })}
+            multiple
           />
         </div>
         <div className='form-group'>
           <label>edit permissions</label>
-          <MultiSelect
-            title='Use the checkboxes to select/deselect multiple options'
-            size={7}
-            value={entity.editPermissions || []}
-            onChange={(selectData) => onChange({ _id: entity._id, editPermissions: selectValues(selectData) })}
-            options={users.map(u => ({ key: u._id, name: u.username, value: u._id }))}
+          <EntityRefSelect
+            headingLabel='Select user (edit permissions)'
+            filter={(references) => {
+              const users = references.users.filter((e) => !e.__isNew)
+              return { users: users }
+            }}
+            value={entity.editPermissions ? entity.editPermissions.map((_id) => Studio.getEntityById(_id, false).shortid) : []}
+            onChange={(selected) => onChange({ _id: entity._id, editPermissions: selectValues(selected) })}
+            multiple
           />
         </div>
       </div>
