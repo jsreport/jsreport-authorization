a3ee033501c5ffa95d5bc8dd6a09678684860a59
BJR Matos <bjrmatos@gmail.com>
2018-12-31 14:46:05 -0500

    remove invalid user references

--- studio/PermissionProperties.js
+++ studio/PermissionProperties.js
@@ -8,12 +8,56 @@ const selectValues = (selected) => {
 }
 
 export default class PermissionProperties extends Component {
+  componentDidMount () {
+    this.removeInvalidUserReferences()
+  }
+
+  componentDidUpdate () {
+    this.removeInvalidUserReferences()
+  }
+
+  removeInvalidUserReferences () {
+    const { entity, onChange } = this.props
+
+    if (Array.isArray(entity.readPermissions) && entity.readPermissions.length > 0) {
+      const updatedReadPermissions = entity.readPermissions.map((_id) => {
+        const currentEntity = Studio.getEntityById(_id, false)
+        return currentEntity ? currentEntity._id : null
+      }).filter((i) => i != null)
+
+      if (updatedReadPermissions.length !== entity.readPermissions.length) {
+        onChange({ _id: entity._id, readPermissions: updatedReadPermissions })
+      }
+    }
+
+    if (Array.isArray(entity.editPermissions) && entity.editPermissions.length > 0) {
+      const updatedEditPermissions = entity.editPermissions.map((_id) => {
+        const currentEntity = Studio.getEntityById(_id, false)
+        return currentEntity ? currentEntity._id : null
+      }).filter((i) => i != null)
+
+      if (updatedEditPermissions.length !== entity.editPermissions.length) {
+        onChange({ _id: entity._id, editPermissions: updatedEditPermissions })
+      }
+    }
+  }
+
   render () {
     const { entity, onChange } = this.props
 
     if (entity.__entitySet === 'users') {
       return <div />
     }
+
+    const readPermissionsEntities = entity.readPermissions ? entity.readPermissions.map((_id) => {
+      const currentEntity = Studio.getEntityById(_id, false)
+      return currentEntity != null ? currentEntity : null
+    }).filter((i) => i != null) : []
+
+    const editPermissionsEntities = entity.editPermissions ? entity.editPermissions.map((_id) => {
+      const currentEntity = Studio.getEntityById(_id, false)
+      return currentEntity != null ? currentEntity : null
+    }).filter((i) => i != null) : []
 
     return (
       <div className='properties-section'>
@@ -25,7 +69,7 @@ export default class PermissionProperties extends Component {
               const users = references.users.filter((e) => !e.__isNew)
               return { users: users }
             }}
-            value={entity.readPermissions ? entity.readPermissions.map((_id) => Studio.getEntityById(_id, false).shortid) : []}
+            value={readPermissionsEntities.map((r) => r.shortid)}
             onChange={(selected) => onChange({ _id: entity._id, readPermissions: selectValues(selected) })}
             multiple
           />
@@ -38,7 +82,7 @@ export default class PermissionProperties extends Component {
               const users = references.users.filter((e) => !e.__isNew)
               return { users: users }
             }}
-            value={entity.editPermissions ? entity.editPermissions.map((_id) => Studio.getEntityById(_id, false).shortid) : []}
+            value={editPermissionsEntities.map((e) => e.shortid)}
             onChange={(selected) => onChange({ _id: entity._id, editPermissions: selectValues(selected) })}
             multiple
           />
